0bb19b164cc547f95a12b94f3747b232479a8128
Viktoria <101473740+vzubarieva@users.noreply.github.com>
2022-06-02 21:24:31 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+// import Triangle from './js/triangle.js';
+
+// $(function () {
+//   $('#triangle-checker-form').on("submit", function (event) {
+//     event.preventDefault();
+//     console.log('hello');
+//     const length1 = parseInt($('#length1').val());
+//     const length2 = parseInt($('#length2').val());
+//     const length3 = parseInt($('#length3').val());
+//     const triangle = new Triangle(length1, length2, length3);
+//     const response = triangle.checkType();
+//     $('#response').append(`<p>${response}</p>`);
+//   });
+ 
+// let request = new XMLHttpRequest();
+// const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
